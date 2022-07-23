57dc09e36f1e5d94daa52025a7a8b58772a15512
jzzheng <justinzheng@ascentiumcapital.com>
2022-07-22 21:57:49 -0500

    oops replace with correct content

--- JS Snippets/MwoClaimLootBags.js
+++ JS Snippets/MwoClaimLootBags.js
@@ -1,23 +1,5 @@
 /*
-
-Mark read up to here in Feedly
-
-find the 'mark as read' button on the element *up to and including which*
-you want to mark articles read, store as global variable, then call like
-markReadUpToHere(temp1)
-
-Will mark all articles as read if bad eltUntil argument is provided!
-This would be the best one for an extension... to just add a button that handles this
-so there's no need to monkey around with element inspector
-
+short and sweet; go to MWO page with a bunch of 'reach in the bag' buttons and run the below
+https://mwomercs.com/tournaments
 */
-
-function markReadUpToHere(eltUntil) {	
-	for (let readBtn of document.querySelectorAll("[aria-label='Mark as Read']"))
-	{
-		readBtn.click();
-		if (readBtn === eltUntil) {
-			break;
-		}
-	}
-}
+setInterval(function() { $('.bonusButton, .reachButton').first().click(); }, 200);
