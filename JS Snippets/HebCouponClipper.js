2b5b5b3cacb5cfc1fd38b1abf92fa64c210505fa
jzzheng <justinzheng@ascentiumcapital.com>
2024-07-11 17:14:09 -0500

    page view compatibility

--- JS Snippets/HebCouponClipper.js
+++ JS Snippets/HebCouponClipper.js
@@ -9,36 +9,64 @@ Brittle - depends on classes etc. being applied consistently.
 NB: the count clipped is based on the number of times a coupon is *clicked*. 
   Sometimes the click event doesn't result in a coupon being clipped, in which case the final ctClipped report from talky will be an overestimate
   
-NB2: HEB seems to have rolled out or at least begun A/B testing a paged view for coupons, 
-  which limits the effectiveness of this script as you have to go to the next page and rerun it. Will have to ponder an update.
+NB2: The paged view only pages through the top X coupons; included a warning about addressing that.
+  Also, not sure if I broke the logic on the original 'infinite view more' version; guess we'll see.
+  In order to get rid of the need to manually refresh & rerun, will need the script to persist between "true" pages. Maybe either a browser extension or do something wacky with generating an iframe.
 
 */
 
-var timesNoViewMore = 0;
+var timesNoNext = 0;
 var ctClipped = 0;
-var talky = alert;	// swap with console.log for less verbosity
-var it = setInterval(function() { 
-	// first try to get all the 'view more' options
-	var viewMore = Array.from(document.querySelectorAll('[color=curbsideblue], [variant=outline]')).filter(function(elt) { return elt.innerText == 'View more'; });
-	if (viewMore.length) {
-		// found view more button; click it
-		viewMore[0].click();
-		timesNoViewMore = 0;
-		return;
+const talky = alert;	// swap with console.log for less verbosity
+const isPaginated = document.querySelectorAll("[data-qe-id=paginationList]").length > 0;
+// try to get next coupons; return true if there appear to have been more to get
+function getNextCoupons() {
+	var next;
+	if (isPaginated) {
+		// 'next' is next page button
+		next = document.querySelectorAll("[data-qe-id=paginationNext]");
+	}
+	else {
+		// 'next' is the 'view more' button
+		next = Array.from(document.querySelectorAll('[color=curbsideblue], [variant=outline]')).filter(function(elt) { return elt.innerText == 'View more'; });
+	}
+	
+	if (next.length) {
+		// found a 'next' button; click it
+		next[0].click();
+		timesNoNext = 0;
+		return true;
+	}
+	
+	return false;
+}
+
+const it = setInterval(function() { 
+	if (!isPaginated) {
+		// when not paginated, first try to pull up all the coupons
+		if (getNextCoupons()) {
+			// were able to get next coupons; end for now (let the next coupons get loaded)
+			return;
+		}
 	}
 
-	// no view more; let's clip a coupon (admittedly view more might be loading but whatever)
-	var coupons = Array.from(document.querySelectorAll("[data-qe-id=couponCard] button")).filter(function(elt) { return elt.innerText == 'Clip'; }); 
+	// let's clip a coupon (admittedly view more might be loading but whatever)
+	const coupons = Array.from(document.querySelectorAll("[data-qe-id=couponCard] button")).filter(function(elt) { return elt.innerText == 'Clip'; }); 
 	if (coupons.length) {
-		var coupon = coupons[0];
+		const coupon = coupons[0];
 		coupon.scrollIntoViewIfNeeded();
 		coupon.click();
 		++ctClipped;
 	}
-	else if ((++timesNoViewMore) > 10) {
-		// neither coupons nor 'view more' button, multiple times - 
+	else if ((++timesNoNext) > 10) {
+		// neither coupons nor paginated nor 'view more' button, multiple times - 
 		// we're done; have encountered more than 10 times with no 'view more' (or coupons)
 		clearInterval(it);
-		talky(`all done! clipped ${ctClipped} coupons`);
+		talky(`all done! clipped ${ctClipped} coupons. `  + (isPaginated ? "Coupons were paginated so process may have to be repeated; remember to restart from a fresh URL / strip the cursor argument from the URL before refreshing." : ""));
+	}
+	else if (isPaginated) {
+		// no coupons left to clip, we haven't hit the "no next" stopping point, and we've got a page view going
+		getNextCoupons();
+		return;
 	}
 }, 1000);
